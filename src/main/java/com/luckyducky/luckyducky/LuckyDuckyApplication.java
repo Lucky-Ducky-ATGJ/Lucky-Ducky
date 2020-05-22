8881d145a99d5043754b56546e1b7a7b96925bee
Ted Zamarron <ted.zamarron@gmail.com>
2020-05-22 09:39:58 -0500

    first commit

---
+++ src/main/java/com/luckyducky/luckyducky/LuckyDuckyApplication.java
@@ -0,0 +1,13 @@
+package com.luckyducky.luckyducky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyDuckyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyDuckyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luckyducky/luckyducky/LuckyDuckyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyducky.luckyducky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyDuckyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
