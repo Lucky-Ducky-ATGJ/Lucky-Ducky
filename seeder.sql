ae65cef0453024ea0ba2928a4b2c3ec4ec926aa3
Ted Zamarron <ted.zamarron@gmail.com>
2020-05-28 10:38:14 -0500

    added a seeder file, created Bill repo, controller and added Bill directory with index html

---
+++ seeder.sql
@@ -0,0 +1,15 @@
+use thePond_db;
+
+INSERT INTO category (id, name, transaction_id)
+VALUES(1, 'Utilities', 0),
+       (2, 'Entertainment', 0),
+       (3, 'Food/Dining',0),
+       (4, 'Fuel', 0),
+       (5, 'Lodging', 0),
+       (6, 'Loans', 0),
+       (7, 'Bills', 0),
+       (8, 'Miscellaneous', 0),
+       (9, 'Shopping', 0),
+       (10, 'Groceries', 0),
+       (11, 'Gifts/Donations', 0),
+       (12, 'Personal', 0);
