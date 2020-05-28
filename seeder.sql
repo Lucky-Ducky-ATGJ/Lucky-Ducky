12cb02debd11e17f931d73ed1b576a28e417d531
James Muralles <james.muralles23@gmail.com>
2020-05-28 17:06:08 -0500

    another little push

--- seeder.sql
+++ seeder.sql
@@ -1,15 +1,15 @@
 use thePond_db;
 
-INSERT INTO category (id, name, transaction_id)
-VALUES(1, 'Utilities', 0),
-       (2, 'Entertainment', 0),
-       (3, 'Food/Dining',0),
-       (4, 'Fuel', 0),
-       (5, 'Lodging', 0),
-       (6, 'Loans', 0),
-       (7, 'Bills', 0),
-       (8, 'Miscellaneous', 0),
-       (9, 'Shopping', 0),
-       (10, 'Groceries', 0),
-       (11, 'Gifts/Donations', 0),
-       (12, 'Personal', 0);
+INSERT INTO category (name)
+VALUES('Utilities'),
+       ('Entertainment'),
+       ('Food/Dining'),
+       ('Fuel'),
+       ('Lodging'),
+       ('Loans'),
+       ('Bills'),
+       ('Miscellaneous'),
+       ('Shopping'),
+       ('Groceries'),
+       ('Gifts/Donations'),
+       ('Personal');
