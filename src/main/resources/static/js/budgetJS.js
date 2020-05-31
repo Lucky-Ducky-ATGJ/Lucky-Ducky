8709f439687c275eb0771dd3ebb963840d9856c2
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-05-31 17:37:32 -0500

    updated budget page

--- src/main/resources/static/js/budgetJS.js
+++ src/main/resources/static/js/budgetJS.js
@@ -1,6 +1,9 @@
 "use strict";
     function calculateBalance(){
-        var remainingBalance = document.getElementById('income').value - document.getElementById('expenditures').value;
+        let remainingBalance = document.getElementById('income').value - document.getElementById('expenditures').value;
         document.getElementById('balance').innerHTML = "$" + remainingBalance.toFixed(2);
     }
 
+    function importIncome(){
+        let getIncome = "SELECT sum(amount_in_cents) as totalIncome FROM transaction as t WHERE t.is_income = true";
+    }
