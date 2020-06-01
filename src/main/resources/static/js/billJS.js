68b4bf684605dacf717e68d5166bf8586f524e30
Ted Zamarron <ted.zamarron@gmail.com>
2020-05-31 21:20:19 -0500

    added comments to JS

--- src/main/resources/static/js/billJS.js
+++ src/main/resources/static/js/billJS.js
@@ -4,8 +4,8 @@
 $('#deleteBill').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var curId = button.data('i') // Current bill id
-    var modal = $(this)
-    modal.find('.modal-footer button#delete-id').val(curId)
+    var modal = $(this) // current modal
+    modal.find('.modal-footer button#delete-id').val(curId) // set bill id to value in button
 });
 
 $('#editBill').on('show.bs.modal', function (event) {
@@ -13,12 +13,12 @@ $('#editBill').on('show.bs.modal', function (event) {
     var name = button.parent().parent().children(":nth-child(1)").text() // Current bill name
     var date = button.parent().parent().children(":nth-child(2)").text() // Current bill date
     var amount = button.parent().parent().children(":nth-child(3)").text() // Current bill amount
-    var id = button.data("id")
-    var modal = $(this)
-    modal.find(".modal-body input#edit-name").val(name);
-    modal.find(".modal-body input#edit-due-date").val(date);
-    modal.find(".modal-body input#edit-amount").val(amount);
-    modal.find(".modal-body input#edit-id").val(id);
+    var id = button.data("id") // Current bill id
+    var modal = $(this) // current modal
+    modal.find(".modal-body input#edit-name").val(name); // set bill name to show in input
+    modal.find(".modal-body input#edit-due-date").val(date); // set bill duedate to show in input
+    modal.find(".modal-body input#edit-amount").val(amount); // set bill amount to show in input
+    modal.find(".modal-body input#edit-id").val(id); // set bill id to value in hidden input
 });
 
 
